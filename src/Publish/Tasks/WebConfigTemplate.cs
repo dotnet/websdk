83f931aeaa5b56d178d54eaabee7c3cdf1a41264
Matt Mitchell <mmitche@microsoft.com>
2020-06-08 10:41:00 -0700

    Archive master branch of websdk (#1500)

--- src/Publish/Tasks/WebConfigTemplate.cs
+++
@@ -1,7 +0,0 @@
-namespace Microsoft.NET.Sdk.Publish.Tasks
-{
-    public static class WebConfigTemplate
-    {
-        public const string Template =  @"<configuration><location path=""."" inheritInChildApplications=""false"" /></configuration>";
-    }
-}
